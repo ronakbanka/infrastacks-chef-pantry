6e7fb8d9a449100496f48155498f1907afa6f103
Murali Raju <murali.raju@appliv.com>
2013-09-07 09:03:33 -0400

    mesos attributes

--- cookbooks/appliv-io-cdh4/attributes/appliv-io-cdh4.rb
+++ cookbooks/appliv-io-cdh4/attributes/appliv-io-cdh4.rb
@@ -1,6 +1,13 @@
 ## Appliv-DS
-default[:appliv_io_cdh4][:wget_path] = "https://s3.amazonaws.com/infrastacks/appliv-io-cdh4_0.0.1-beta_amd64.deb"
+#Spark
+default[:appliv_io_cdh4][:wget_path] = "http://localhost:3000/appliv-io-cdh4_0.0.1-beta_amd64.deb"
 default[:appliv_io_cdh4][:dist] = "appliv-io-cdh4"
-default[:appliv_io_cdh4][:spark][:master] = "hadoop-cdh4-node1"
+default[:appliv_io_cdh4][:spark][:master] = "appliv-io-node1"
 default[:appliv_io_cdh4][:spark][:home] = "/opt/appliv-io-cdh4/component/spark-0.7.3"
 default[:appliv_io_cdh4][:scala][:home] = "/opt/appliv-io-cdh4/deps/scala-2.9.3"
+
+#Mesos
+default[:appliv_io_cdh4][:mesos][:home] = "/usr/local/mesos"
+default[:appliv_io_cdh4][:mesos][:master] = "appliv-io-node1"
+default[:appliv_io_cdh4][:mesos][:slaves_prefix] = "appliv-io-node" 
+default[:appliv_io_cdh4][:mesos][:ssh_key_options] = "key"

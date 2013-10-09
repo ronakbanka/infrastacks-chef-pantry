2949670348259b77499ddd2b58e9749e81e446ec
Murali Raju <murali.raju@appliv.com>
2013-10-09 18:32:04 -0400

    build scaffold

--- cookbooks/appliv-io-cdh4/attributes/appliv-io-cdh4.rb
+++ cookbooks/appliv-io-cdh4/attributes/appliv-io-cdh4.rb
@@ -5,6 +5,9 @@ default[:appliv_io_cdh4][:dist] = "appliv-io-cdh4"
 default[:appliv_io_cdh4][:spark][:master] = "appliv-io-node1"
 default[:appliv_io_cdh4][:spark][:home] = "/opt/appliv-io-cdh4/component/spark-0.8.0"
 default[:appliv_io_cdh4][:scala][:home] = "/opt/appliv-io-cdh4/deps/scala-2.9.3"
+default[:appliv_io_cdh4][:dist] = "0.8.0"
+default[:appliv_io_cdh4][:wget_path] = "http://spark-project.org/download/spark-0.8.0-incubating.tgz"
+
 
 #Mesos
 default[:appliv_io_cdh4][:mesos][:home] = "/usr/local/mesos"
--- cookbooks/appliv-io-cdh4/recipes/build.rb
+++ cookbooks/appliv-io-cdh4/recipes/build.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: appliv-io-cdh4
-# Recipe:: [Setup Appliv-IO]
+# Recipe:: [Setup Appliv-IO Package Builder]
 #
 # Copyright 2013, InfraStacks,LLC  engineering@infrastacks.com
 #
@@ -20,5 +20,32 @@ package "upstart"
 package "libunwind7"
 package "libunwind7-dev"
 
+gem_package "fpm"
+
 include_recipe "java::oracle"
 
+appliv_io_spark_dist = node[:appliv_io_cdh4][:dist]
+appliv_io_spark_wget_path = node[:appliv_io_cdh4][:wget_path]
+
+remote_file "/tmp/spark-#{appliv_io_spark_dist}.tgz" do
+  source "#{appliv_io_spark_wget_path}"
+  not_if { File.exists?("/tmp/spark-#{appliv_io_spark_dist}.tgz") }
+end
+
+script "Setup Build Environment" do
+  interpreter "bash"
+  code <<-EOH
+  mkdir -p /home/vagrant/appliv-io-cdh4-build
+  tar -zxvf /tmp/spark-#{appliv_io_spark_dist}.tgz -C /home/vagrant/appliv-io-cdh4-build
+  EOH
+  not_if { File.exists?("/home/vagrant/appliv-io-cdh4-build/spark-0.8.0-incubating") }
+end
+
+script "Building Spark" do
+  interpreter "bash"
+  code <<-EOH
+  mkdir -p /home/vagrant/appliv-io-cdh4-build
+  tar -zxvf /tmp/spark-#{appliv_io_spark_dist}.tgz -C /home/vagrant/appliv-io-cdh4-build
+  EOH
+  not_if { File.exists?("/home/vagrant/appliv-io-cdh4-build/spark-0.8.0-incubating") }
+end
